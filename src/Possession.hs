04ed6f045b7ceeeb8f03dd7226c75c37a53312b1
Patrick Thomson <patrickt@github.com>
2020-09-24 16:08:58 -0400

    brick event loop

--- src/Possession.hs
+++ src/Possession.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-
 module Possession (main) where
 
 import Brick qualified
+import Brick.BChan
+import Graphics.Vty qualified as Vty
 
 ui :: Brick.Widget ()
 ui = Brick.str "Possession, v0.0.0"
 
 main :: IO ()
-main = Brick.simpleMain ui
+main = do
+  brickEvLoop <- newBChan 1
+  vty <- Vty.standardIOConfig >>= Vty.mkVty
+  Brick.customMain vty (pure vty) (Just brickEvLoop) (Brick.simpleApp ui) ()
