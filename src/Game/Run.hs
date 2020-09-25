2a569d4d8be41f7568156aada1eb06cc5eef19cd
Patrick Thomson <patrickt@github.com>
2020-09-24 22:15:23 -0400

    involve the world in the loop

--- src/Game/Run.hs
+++ src/Game/Run.hs
@@ -3,6 +3,7 @@
 
 module Game.Run (start) where
 
+import Apecs qualified
 import Brick.BChan
 import Control.Algebra qualified as Eff
 import Control.Carrier.Reader hiding (Has)
@@ -10,21 +11,25 @@ import Control.Concurrent
 import Control.Monad
 import Control.Monad.IO.Class
 import Game.Action
+import Game.World qualified as Game (World)
 
 type Command = ()
 
 loop ::
-  (MonadIO m, Eff.Has (Reader (MVar Action)) sig m) =>
-  m ()
+  ( MonadIO m,
+    Eff.Has (Reader (MVar Action)) sig m
+  ) =>
+  Apecs.SystemT Game.World m ()
 loop = do
   _next <- ask >>= liftIO . takeMVar @Action
   pure ()
 
-start :: BChan Command -> MVar Action -> IO ()
-start cmds acts =
+start :: BChan Command -> MVar Action -> Game.World -> IO ()
+start cmds acts world =
   void
     . forkIO
     . runReader cmds
     . runReader acts
+    . flip Apecs.runSystem world
     . forever
     $ loop
--- src/Game/World.hs
+++ src/Game/World.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Game.World (World (..), initWorld) where
 
 import Apecs
+import Control.Algebra qualified as Eff
+import Control.Carrier.Reader qualified as Eff
 import Linear (V2 (..))
 
 newtype Position = Position (V2 Double) deriving stock (Show)
@@ -17,3 +23,7 @@ newtype Glyph = Glyph Char deriving newtype (Show)
 data Color = Black | Grey | White deriving (Show)
 
 makeWorldAndComponents "World" [''Position, ''Glyph, ''Color]
+
+deriving newtype instance
+  Eff.Algebra sig m =>
+  Eff.Algebra (Eff.Reader World Eff.:+: sig) (SystemT World m)
