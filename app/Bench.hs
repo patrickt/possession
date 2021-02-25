7e732afd2398e0b9b7df726b97c8ae6f4ed71dbc
Patrick Thomson <patrickt@github.com>
2021-02-25 13:56:44 -0500

    gauge is better

--- app/Bench.hs
+++ app/Bench.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Test.Tasty.Bench
+import Gauge
 import Game.Dungeon
 import Data.Monoid
 import Control.Comonad
@@ -9,17 +9,13 @@ import Data.Foldable
 iter :: Game -> Int -> Game
 iter g n = appEndo (fold (replicate n (Endo (extend step)))) g
 
-fibo :: Int -> Integer
-fibo n = if n < 2 then toInteger n else fibo (n - 1) + fibo (n - 2)
-
-myFibo :: Int -> Integer
-myFibo n = if n < 3 then toInteger n else myFibo (n - 1) + myFibo (n - 2)
-
 main :: IO ()
 main = do
   x <- randomly
-  Test.Tasty.Bench.defaultMain -- not Test.Tasty.defaultMain
-    [ bench "iter 20" $ nf (iter x) 20
-    , bench "iter 40" $ nf (iter x) 40
-    , bench "iter 60" $ nf (iter x) 60
+  let y = iter x
+  defaultMain [
+    bgroup "fib" [ bench "10" $ nf y 20
+                 , bench "35" $ nf y 40
+                 , bench "37" $ nf y 60
+                 ]
     ]
