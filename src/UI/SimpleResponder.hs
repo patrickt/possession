0e3ca268972efd5ed2471de9bbbbd860f553bd17
Patrick Thomson <patrickt@github.com>
2021-11-18 09:17:41 -0500

    doesn't work for unclear reasons

--- src/UI/SimpleResponder.hs
+++ src/UI/SimpleResponder.hs
@@ -4,17 +4,21 @@ module UI.SimpleResponder
   ( Path (..)
   , propagateChanges
   , findActions
+  , recurse
+  , runResponder
   , Responder (..)
   ) where
 
 import Game.Action
 import Optics
 
+class Responder a where respondTo :: a -> Path a
+
 data Path a where
-  Ok :: Path a
+  Ok :: a -> Path a
   Fail :: Path a
   Alt :: Path a -> Path a -> Path a
-  Try :: Responder b => (a -> Maybe b) -> (a -> b -> a) -> Path a
+  Try :: Responder b => (a -> Maybe b) -> (b -> a -> a) -> Path a
   Emit :: GameAction -> Path a -> Path a
 
 instance Semigroup (Path a) where
@@ -25,28 +29,39 @@ instance Semigroup (Path a) where
 instance Monoid (Path a) where
   mempty = Fail
 
-class Responder a where respondTo :: a -> Path a
+instance Responder a => Responder (Maybe a) where
+  respondTo = maybe mempty (go . respondTo)
+    where
+      go = \case
+        Ok a -> Ok (Just a)
+        Fail -> Fail
+        Alt l r -> Alt (go l) (go r)
+        Emit act q -> Emit act (go q)
+        Try getter setter -> Try (>>= getter) (fmap . setter)
 
 propagateChanges :: Responder a => Path a -> a -> a
 propagateChanges p = case p of
-  Ok -> id
+  Ok x -> const x
   Fail -> id
   Try fn setter -> \x ->
     case fn x of
       Nothing -> x
-      Just val -> setter x (propagateChanges (respondTo val) val)
+      Just val -> setter (runResponder val) x
   Alt Fail p' -> propagateChanges p'
   Alt p' _ -> propagateChanges p'
   Emit _ p' -> propagateChanges p'
 
+runResponder :: Responder a => a -> a
+runResponder x = propagateChanges (respondTo x) x
+
 recurse :: (k `Is` An_AffineFold, k `Is` A_Setter, Responder child) => Optic' k is parent child -> Path parent
-recurse opt = Try (preview opt) (\x y -> x & opt %~ propagateChanges (respondTo y))
+recurse opt = Try (preview opt) (over opt . propagateChanges . respondTo)
 
 findActions :: Path a -> a -> [GameAction]
 findActions p x = go p []
   where
     go (Emit act p') = go p' . (act:)
-    go Ok = id
+    go (Ok _) = id
     go Fail = id
     go (Alt Fail p') = go p'
     go (Alt p' _) = go p'
--- test/Test.hs
+++ test/Test.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Main where
 
 import Hedgehog
@@ -12,7 +13,10 @@ import Data.Foldable
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
 import Data.Monoid
+import Optics
+import Optics.TH
 import Data.Vector.Zipper qualified as Z
+import UI.SimpleResponder
 
 prop_zipperRotatesCancelOut :: Property
 prop_zipperRotatesCancelOut = property do
@@ -21,11 +25,39 @@ prop_zipperRotatesCancelOut = property do
   let make = fmap Endo . replicate times
   appEndo (fold (make Z.shiftLeft <> make Z.shiftRight)) z === z
 
-prop_zipperRotateAllTheWayAround :: Property
-prop_zipperRotateAllTheWayAround = property do
-  z <- forAll . Gen.zipper . Gen.vector (Range.linear 1 100) . Gen.int $ Range.linear 1 100
-  let shifts = replicate (length z) Z.shiftLeft
-  appEndo (foldMap Endo shifts) z === z
+-- prop_zipperRotateAllTheWayAround :: Property
+-- prop_zipperRotateAllTheWayAround = property do
+--   z <- forAll . Gen.zipper . Gen.vector (Range.linear 1 100) . Gen.int $ Range.linear 1 100
+--   let shifts = replicate (length z) Z.shiftLeft
+--   appEndo (foldMap Endo shifts) z === z
+
+data ModalTest = ModalTest { menu :: MenuTest, canvas :: CanvasTest }
+  deriving stock (Eq, Show)
+
+data MenuTest = Closed | Open
+  deriving stock (Eq, Show)
+
+instance Responder MenuTest where
+  respondTo Closed = Ok Open
+  respondTo Open = Ok Closed
+
+data CanvasTest = CanvasTest
+  deriving stock (Eq, Show)
+
+instance Responder CanvasTest where
+  respondTo = mempty
+
+makeFieldLabelsNoPrefix ''ModalTest
+
+instance Responder ModalTest where
+  respondTo _ = recurse #menu <> recurse #canvas
+
+prop_simpleResponderTestsWork :: Property
+prop_simpleResponderTestsWork = withTests 1 $ property do
+  runResponder Closed === Open
+  runResponder Open === Closed
+  let mt = ModalTest Open CanvasTest
+  runResponder mt === ModalTest Closed CanvasTest
 
 main :: IO ()
 main = void (checkParallel $$(discover))
