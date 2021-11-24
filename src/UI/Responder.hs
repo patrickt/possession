0417fe08d6df281decc293dcd7501c146404bcba
Patrick Thomson <patrickt@github.com>
2021-11-24 13:20:22 -0500

    shallow embedding doesn't work

--- src/UI/Responder.hs
+++ src/UI/Responder.hs
@@ -15,7 +15,6 @@ module UI.Responder
     try,
     recurse,
     eval,
-    actions,
     switch,
     result,
     emptyResult,
@@ -27,12 +26,15 @@ import Game.Action (GameAction)
 import Graphics.Vty qualified as Vty
 import Optics
 import UI.Event
+import Control.Alternative.Free
 import Control.Applicative
-import Control.Applicative.Free
 import Control.Arrow
 import Data.Monoid (First (..))
+import Data.Monoid qualified as Monoid
+import Control.Monad
 
-data Result a = Ok a | Fail | Emit GameAction a
+data Result a = Ok a | Fail | Emit [GameAction] a
+  deriving stock Functor
 
 instance Semigroup (Result a) where
   Fail <> a = a
@@ -40,20 +42,36 @@ instance Semigroup (Result a) where
 
 instance Monoid (Result a) where mempty = Fail
 
+instance Applicative Result where
+  pure = Ok
+  Ok f <*> Ok a = Ok (f a)
+  Ok f <*> Emit e a = Emit e (f a)
+  Emit e f <*> Ok a = Emit e (f a)
+  Emit e f <*> Emit e' a = Emit (e <> e') (f a)
+  Fail <*> _ = Fail
+  _ <*> Fail = Fail
+
+deriving via Monoid.Alt Result instance Alternative Result
+
 emptyResult :: Monoid a => Path a
 emptyResult = pure mempty
 
-newtype Path a = Path { runPath :: Vty.Event -> a -> Result a }
+newtype PathF a = PathF { runPathF :: Vty.Event -> a -> Result a }
 
+newtype Path a = Path { runPath :: Alt PathF a }
+  deriving newtype (Functor, Applicative, Alternative)
+
+liftPathF :: PathF a -> Path a
+liftPathF = Path . liftAlt
 
 result :: a -> Path a
 result = pure
 
 switch :: (Vty.Event -> a -> Result a) -> Path a
-switch = Path
+switch fn = Path (liftAlt (PathF fn))
 
 emitting :: a -> GameAction -> Result a
-emitting = flip Emit
+emitting a e = Emit (pure e) a
 
 -- Coalgebra describing how components unfold into an event tree
 class Responder a where respondTo :: a -> Path a
@@ -72,7 +90,7 @@ try ::
 try getit setit a = recurse setit a <* guard (has getit a)
 
 whenMatches :: (Is query A_Fold) => Optic' query is (Event () a) unused -> (a -> Result a) -> a -> Path a
-whenMatches opt fn a = Path (\e _ -> if has opt (Event e a ()) then fn a else Fail)
+whenMatches opt fn a = switch $ (\e _ -> if has opt (Event e a ()) then fn a else Fail)
 
 overState :: Is k A_Fold => Optic' k is (Event () a) contents -> (a -> a) -> a -> Path a
 overState opt fn = whenMatches opt (Ok . fn)
@@ -80,5 +98,6 @@ overState opt fn = whenMatches opt (Ok . fn)
 recurse :: (Is k A_Traversal, Responder b) => Optic' k is a b -> a -> Path a
 recurse opt = traverseOf opt respondTo
 
+
 eval :: forall a. Path a -> Vty.Event -> a -> Result a
-eval = runPath
+eval (Path (Alt xs)) = _
